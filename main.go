b550cdcf330769038ce850b85f77fb52dad6ac22
taguchi kazuki <elfubolfelicidadsuprema02@gmail.com>
2023-07-10 00:44:58 +0900

    add: channel unbuffer buffer goroutine leak

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
-	"os"
 	"runtime"
 	"runtime/trace"
 	"sync"
@@ -38,50 +36,85 @@ func main() {
 	// fmt.Printf("num of working goroutines: %d\n", runtime.NumGoroutine())
 	// fmt.Println("main func finish")
 	// #### tracer ####
-	f, err := os.Create("trace.out")
-	if err != nil {
-		log.Fatalln("Error:", err)
-	}
-	defer func() {
-		// if文の中でerrを宣言して条件分岐として使うこともできる
-		if err := f.Close(); err != nil {
-			log.Fatalln("Error:", err)
-		}
+	// f, err := os.Create("trace.out")
+	// if err != nil {
+	// 	log.Fatalln("Error:", err)
+	// }
+	// defer func() {
+	// 	// if文の中でerrを宣言して条件分岐として使うこともできる
+	// 	if err := f.Close(); err != nil {
+	// 		log.Fatalln("Error:", err)
+	// 	}
+	// }()
+	// // トレースの開始
+	// if err := trace.Start(f); err != nil {
+	// 	log.Fatalln("Error:", err)
+	// }
+	// defer trace.Stop()
+	// // トレースの実際の処理 mainという名前でトレースを開始する
+	// ctx, t := trace.NewTask(context.Background(), "main")
+	// defer t.End()
+	// fmt.Println("The number of logical CPU Cores:", runtime.NumCPU())
+	// // 逐次処理の場合
+	// // task(ctx, "Task1")
+	// // task(ctx, "Task2")
+	// // task(ctx, "Task3")
+	// // 並列処理の場合
+	// var wg sync.WaitGroup
+	// wg.Add(3)
+	// go cTask(ctx, &wg, "Task1")
+	// go cTask(ctx, &wg, "Task2")
+	// go cTask(ctx, &wg, "Task3")
+	// s := []int{1, 2, 3}
+	// // goroutineの中だと起動に少し時間がかかりiの値が更新されて出力されてしまう。
+	// // そのため、goroutineの中でiを使う場合は引数として渡す必要がある
+	// // goroutineでは完全に別のスレッドで実行されるため、順番が前後することがある
+	// for _, i := range s {
+	// 	wg.Add(1)
+	// 	// go func() {
+	// 	go func(i int) {
+	// 		defer wg.Done()
+	// 		fmt.Println(i)
+	// 	}(i)
+	// 	// }()
+	// }
+	// wg.Wait()
+	// fmt.Println("main func finish")
+
+	// ### goroutine:channel ###
+	// チャネルを使うことによって、goroutine間でデータのやり取りを行うことができる
+	// チャネルへの書き込み操作は、チャネルへの読み込み操作が行われるまでブロックされる(バッファがない場合)
+	// やりとりするデータの型を指定する必要がある 下記ではint型
+	// <- はチャネルの読み込み操作 / / ch <- はチャネルへの書き込み操作
+	// 別のgoroutineの値をチャネル経由で受け取る
+	// ch := make(chan int)
+	// var wg sync.WaitGroup
+	// wg.Add(1)
+	// go func() {
+	// 	defer wg.Done()
+	// 	ch <- 10
+	// 	time.Sleep(500 * time.Millisecond)
+	// }()
+	// fmt.Println(<-ch)
+	// wg.Wait()
+	// fmt.Println("main func finish")
+	// goroutineリーク 受信側が送信されるまで待ち続けるため、メモリが解放されないことを言う
+	// 以下のようにgoroutineを作成すると、受信側が送信されるまで待ち続けるため、goroutineが終了しない
+	ch1 := make(chan int)
+	go func() {
+		fmt.Println(<-ch1)
 	}()
-	// トレースの開始
-	if err := trace.Start(f); err != nil {
-		log.Fatalln("Error:", err)
-	}
-	defer trace.Stop()
-	// トレースの実際の処理 mainという名前でトレースを開始する
-	ctx, t := trace.NewTask(context.Background(), "main")
-	defer t.End()
-	fmt.Println("The number of logical CPU Cores:", runtime.NumCPU())
-	// 逐次処理の場合
-	// task(ctx, "Task1")
-	// task(ctx, "Task2")
-	// task(ctx, "Task3")
-	// 並列処理の場合
-	var wg sync.WaitGroup
-	wg.Add(3)
-	go cTask(ctx, &wg, "Task1")
-	go cTask(ctx, &wg, "Task2")
-	go cTask(ctx, &wg, "Task3")
-	s := []int{1, 2, 3}
-	// goroutineの中だと起動に少し時間がかかりiの値が更新されて出力されてしまう。
-	// そのため、goroutineの中でiを使う場合は引数として渡す必要がある
-	// goroutineでは完全に別のスレッドで実行されるため、順番が前後することがある
-	for _, i := range s {
-		wg.Add(1)
-		// go func() {
-		go func(i int) {
-			defer wg.Done()
-			fmt.Println(i)
-		}(i)
-		// }()
-	}
-	wg.Wait()
-	fmt.Println("main func finish")
+	// 下記の書き込みの処理がないとgoroutineリークが起きる
+	ch1 <- 10
+	fmt.Printf("num of working goroutines: %d\n", runtime.NumGoroutine())
+	// バッファを指定することで、goroutineリークを防ぐことができる
+	// バッファを指定すると、バッファの分だけ書き込みができる(バッファの分だけ読み込みを待たずに書き込みができる)
+	// バッファを超える書き込み or 読み込みから書き込みの順番になると deadlock が起きる
+	ch2 := make(chan int, 1)
+	ch2 <- 2
+	// ch2 <- 3
+	fmt.Println(<-ch2)
+
 }
 
 func task(ctx context.Context, name string) {
