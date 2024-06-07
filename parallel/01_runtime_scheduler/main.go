8ad0dd98fc830f4c15dd021052c38b90c5e8f619
taguchi kazuki <elfubolfelicidadsuprema02@gmail.com>
2024-06-08 00:26:25 +0900

    feat: parallel memo

---
+++ parallel/01_runtime_scheduler/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// 久しぶりに並列処理の勉強し直し
+	// runtime scheduler
+	// golangはgoroutineを使って並列処理を行う
+	// goroutineはスレッドよりも軽量で、数万個のgoroutineを作成しても問題ない
+	// goroutineはruntime schedulerによって管理される
+}
